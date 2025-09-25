594f8331e512580ac297c451bf88632db2ecaa2c
FranckMartial84 <koffikouassifranckmartial@gmail.com>
2025-09-25 15:23:47 +0000

    Jai change l'icon du menu

--- listerese.php
+++ listerese.php
@@ -7,7 +7,9 @@ $clients_chambre  = $req->fetchAll();
 ?>
 <!DOCTYPE html>
 <html lang="fr">    
-<head>
+    
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des réservations</title>
